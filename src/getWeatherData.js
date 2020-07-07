9df10d94f6194cac7f2d55f965ef3c984cff787a
vzdrizhni <vzdrizhni@gmail.com>
2020-07-07 19:32:23 +0500

    Retrive local data

---
+++ src/getWeatherData.js
@@ -0,0 +1,28 @@
+class GetLocalWeatherData {
+  getPosition = (opts) => {
+    const promise = new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((success) => {
+        resolve(success);
+      });
+    });
+    return promise;
+  };
+
+  async weatherData() {
+    const coords = await this.getPosition();
+    const latitude = coords.coords.latitude;
+    const longtitude = coords.coords.longitude;
+    console.log(coords);
+    console.log(latitude, longtitude);
+    const key = '864ff7c45e1d04dd44cff07ac303aa2c';
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${key}`;
+
+    return fetch(url, {
+      method: 'GET'
+    }).then( response => {
+      return response.json;
+    });
+  }
+}
+
+export default GetLocalWeatherData
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import 'bootstrap';
 import './scss/app.scss';
+import GetLocalWeatherData from './getWeatherData'
+
+const weather = new GetLocalWeatherData();
+weather.weatherData();
