407889240ac6b541645a2fca7188fdb6038f2651
vzdrizhni <vzdrizhni@gmail.com>
2020-07-08 14:03:34 +0500

    Switch between celcius and fahrenheit

--- src/render.js
+++ src/render.js
@@ -14,12 +14,22 @@ class RenderWeather {
 
   async render () {
     const weatherDataResult = await this.getData();
-    // console.log(weatherDataResult.main.temp);
+    const switchWeather = document.querySelector('.btn-group')
+    const temp = document.querySelector('.temp');
+
     this.renderCity(weatherDataResult);
-    this.renderTemp(weatherDataResult);
+    temp.innerHTML = this.renderTempCel(weatherDataResult);
     this.renderWind(weatherDataResult);
     this.renderIcon(weatherDataResult);
     this.renderDate(weatherDataResult);
+
+    switchWeather.addEventListener('click', event => {
+      if (event.target.classList.contains('celcius')) {
+        temp.innerHTML = this.renderTempCel(weatherDataResult);
+      } else if (event.target.classList.contains('fahrenheit')) {
+        temp.innerHTML = this.renderTempFah(weatherDataResult);
+      }
+    })
   }
 
   renderCity (result) {
@@ -28,10 +38,17 @@ class RenderWeather {
     city.textContent = weatherResultName;
   }
 
-  renderTemp (result) {
-    const temp = document.querySelector('.temp');
+  renderTempCel (result) {
+    // const temp = document.querySelector('.temp');
     const tempResult =  result.main.temp;
-    temp.innerHTML = Math.round(parseFloat(tempResult)-273.15) + '&deg;';
+    const temp = Math.round(parseFloat(tempResult)-273.15) + 'C' + '&deg;';
+    return temp;
+  }
+
+  renderTempFah (result) {
+    const tempResult =  result.main.temp;
+    const temp = Math.round(((parseFloat(tempResult)-273.15)*1.8)+32) + 'F' + '&deg;';
+    return temp;
   }
 
   renderWind (result) {
